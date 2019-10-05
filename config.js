2a7475da12472c2f95e63d078ec2ce8f706e641d
hossam hassan <hossamhassan889@gmail.com>
2019-10-05 15:38:51 -0400

    make it betterr

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
     ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3000 , 
-    URL: process.env.BASE_URL || 'http://localhost:3000',
+    PORT: process.env.PORT || 80 , 
+    URL: process.env.BASE_URL || 'https://node20520.herokuapp.com',
     MONGODB_URI: process.env.MONGODB_URI ||  
-     'mongo mongodb://localhost:27017/clients?authSource=$[authSource] --username $[username]'
-    // 'mongodb+srv://hossam:hossam1991@cluster0-fnctr.gcp.mongodb.net/clients?retryWrites=true&w=majority'
+    //  'mongo mongodb://localhost:27017/clients?authSource=$[authSource] --username $[username]'
+    'mongodb+srv://hossam:hossam1991@cluster0-fnctr.gcp.mongodb.net/clients?retryWrites=true&w=majority'
    
 }
